29a76741b11c67aa24ad37a7541e8724da2616b0
Ganfina Brice <ganfinab@gmail.com>
2025-03-27 17:29:05 +0000

    add some features

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ mkdir -pv ~/.config/ ~/documents ~/downloads ~/pictures ~/videos ~/images ~/desk
 (cd ~/repos && git clone https://aur.archlinux.org/yay)
 (cd ~/repos/yay && makepkg -si)
 
-sudo pacman -S cantarell-fonts ttf-fira-code ttf-jetbrains-mono cmake nodejs npm pyqt5  networkmanager intel-ucode alsa-utils polkit picom nitrogen alacritty z xorg-apps xorg-server arandr alsa-utils exa apache nginx youtube-dl xdg-utils alsa xdg-user-dirs xdg-utils lxsession man mpv ttf-sourcecodepro-nerd ttf-hack-nerd firefox firefox slock xss-lock discord brightnessctl blueman isync notmuch scrot gparted rsync wireshark-cli wireshark-qt nmap npm network-manager-applet feh xclip tmux clamav mutt xorg-xhost gnome-terminal
+sudo pacman -S cantarell-fonts ttf-fira-code ttf-jetbrains-mono cmake nodejs npm pyqt5  networkmanager intel-ucode alsa-utils polkit picom nitrogen alacritty z xorg-apps xorg-server arandr alsa-utils exa apache nginx youtube-dl xdg-utils alsa xdg-user-dirs xdg-utils lxsession man mpv ttf-sourcecodepro-nerd ttf-hack-nerd firefox firefox slock xss-lock discord brightnessctl blueman isync notmuch scrot gparted rsync wireshark-cli wireshark-qt nmap npm network-manager-applet feh xclip tmux clamav mutt xorg-xhost gnome-terminal   
 
 git config --global user.name "Ganfina Brice"
 git config --global user.email "ganfinab@gmail.com"
