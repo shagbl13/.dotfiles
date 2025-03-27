1bc77a79f685f413f602058e142e280f80086d14
Ganfina Brice <ganfinab@gmail.com>
2025-03-27 20:50:18 +0000

    fix: install script error fixed

--- .config/nvim/after/plugin/colors.lua
+++ .config/nvim/after/plugin/colors.lua
@@ -1,9 +1,9 @@
-require('tokyonight').setup({
+require('rose-pine').setup({
     disable_background = true
 })
 
 function ColorMyPencils(color) 
-	color = color or "tokyonight-night"
+	color = color or "rose-pine-moon"
 	vim.cmd.colorscheme(color)
 
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
